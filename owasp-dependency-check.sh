ea33ed183a8a0c88192bb3ae88342b1f9db9676f
stevinHi <128479820+stevinHi@users.noreply.github.com>
2023-07-24 13:40:50 +0100

    Update owasp-dependency-check.sh
    quelques modifications

--- owasp-dependency-check.sh
+++ owasp-dependency-check.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
-# Script pour télécharger et exécuter DependencyCheck
-rm owasp* || true
-wget "https://raw.githubusercontent.com/RetireJS/retire.js/master/repository/jsrepository.json"
-wget "https://raw.githubusercontent.com/jeremylong/DependencyCheck/master/bin/dependency-check.sh"
-chmod +x owasp-dependency-check.sh
+
+# Supprimer les fichiers précédents s'ils existent
+rm -rf dependency-check-report.xml
+
+# Exécuter OWASP Dependency-Check via Docker
 docker run --rm -v "$PWD:/report" owasp/dependency-check --scan /report --format "XML" --out /report/dependency-check-report.xml
-cat dependency-check-report.xml
